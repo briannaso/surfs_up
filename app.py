a2e3be12529479581b275c86265c8aeef062e481
briannaso <sosa.brianna8@gmail.com>
2021-10-16 11:00:17 -0400

    created app.py

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-# import the flask dependency
+# 1. import Flask
 from flask import Flask
 
+# 2. Create an app, being sure to pass __name__
 app = Flask(__name__)
+
+# 3. Define what to do when a user goes to the index route
+@app.route("/")
+def hello_world():
+    #print("Server received request for 'Home' page...")
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run(debug=True)
